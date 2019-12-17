de120eb577e1d82f13ab268ee39c4d2cc5a39d75
Jurica Bradaric <jbradaric@gmail.com>
2019-12-18 00:42:28 +0100

    Day 09

---
+++ day_09/src/main.rs
@@ -0,0 +1,26 @@
+use intcode::Intcode;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut output = Vec::new();
+    let mut code: Vec<i64> = {
+        let mut file = File::open(env::args().nth(1).unwrap())?;
+        let mut tmp = String::new();
+        file.read_to_string(&mut tmp)?;
+        tmp.trim()
+            .split(',')
+            .map(|s| s.parse())
+            .map(Result::unwrap)
+            .collect()
+    };
+    code.clone().run(&[1], &mut output)?;
+    println!("Part 1: {}", output[0]);
+
+    output.clear();
+    code.run(&[2], &mut output)?;
+    println!("Part 2: {}", output[0]);
+    Ok(())
+}
